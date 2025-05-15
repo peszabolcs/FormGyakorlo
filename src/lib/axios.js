8397e90b3ef258fe2b2ae3b451688c700ad29eab
Perjési Szabolcs <perjesidev@gmail.com>
2025-05-15 14:56:04 +0200

    add axios

---
+++ src/lib/axios.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+// Create axios instance with default config
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // You can add auth token here if needed
+    // const token = localStorage.getItem('token');
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Handle common errors here
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Response Error:", error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("Request Error:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
