c83a2f6b0310bad6367e7ff763004c6731708f9a
Perjési Szabolcs <perjesidev@gmail.com>
2025-05-19 09:44:29 +0200

    minor fix

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -8,16 +8,16 @@ import {
 } from "@mui/material";
 import { useFormik } from "formik";
 import { useMutation } from "@tanstack/react-query";
-import {
-  validationSchemas,
-  mockLoginApi,
-  type LoginFormValues,
-} from "../formConfig";
+import { validationSchemas, mockLoginApi } from "../formConfig";
+import type { LoginFormValues } from "../formConfig";
+import { useTranslation } from "react-i18next";
+import LanguageSwitcher from "./LanguageSwitcher";
 import { toFormikValidationSchema } from "zod-formik-adapter";
 import { createSession } from "../utils/sessionManager";
 import { useRouter } from "@tanstack/react-router";
 
 export const LoginPage = () => {
+  const { t } = useTranslation();
   const router = useRouter();
 
   const loginMutation = useMutation({
@@ -44,6 +44,7 @@ export const LoginPage = () => {
 
   return (
     <Box>
+      <LanguageSwitcher />
       <Card sx={{ minWidth: 350, boxShadow: 6, borderRadius: 3 }}>
         <CardContent>
           <Typography
@@ -53,11 +54,11 @@ export const LoginPage = () => {
             mb={2}
             color="primary"
           >
-            Bejelentkezés
+            {t("form.login.title")}
           </Typography>
           <form onSubmit={formik.handleSubmit}>
             <TextField
-              label="Felhasználónév"
+              label={t("form.login.username")}
               variant="outlined"
               fullWidth
               margin="normal"
@@ -71,7 +72,7 @@ export const LoginPage = () => {
               autoFocus
             />
             <TextField
-              label="Jelszó"
+              label={t("form.login.password")}
               type="password"
               variant="outlined"
               fullWidth
@@ -93,7 +94,9 @@ export const LoginPage = () => {
               sx={{ mt: 2, borderRadius: 2 }}
               disabled={loginMutation.isPending}
             >
-              {loginMutation.isPending ? "Bejelentkezés..." : "Bejelentkezés"}
+              {loginMutation.isPending
+                ? t("form.login.button") + "..."
+                : t("form.login.button")}
             </Button>
           </form>
         </CardContent>
